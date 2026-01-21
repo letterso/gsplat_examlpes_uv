ecfa53f086429685edb9df1562dd9e554508106f
letterso <lettersony00@gmail.com>
2026-01-21 10:40:55 +0800

    fix: 修复可视化异常

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -1118,8 +1118,8 @@ class Runner:
             far_plane=render_tab_state.far_plane,
             radius_clip=render_tab_state.radius_clip,
             eps2d=render_tab_state.eps2d,
-            backgrounds=torch.tensor(render_tab_state.backgrounds, device=self.device)
-            / 255.0,
+            backgrounds=(torch.tensor(render_tab_state.backgrounds, device=self.device).float()
+            / 255.0).view(1, -1),
             render_mode=RENDER_MODE_MAP[render_tab_state.render_mode],
             rasterize_mode=render_tab_state.rasterize_mode,
             camera_model=render_tab_state.camera_model,
